3f9785eaeecf1018466cbcc7f87c687d447f096b
Francisvan Oliveira <francisvan.oliveira@outlook.com>
2021-11-01 01:26:35 -0400

    Lista de Exercícios em C
    Adicionado as questões 27, 29 e 31

---
+++ Exercícios em C/exercicio27.cpp
@@ -0,0 +1,31 @@
+/*Escreva um programa para ler 2 valores e se o segundo valor informado for ZERO, 
+deve ser lido um novo valor, ou seja, para o segundo valor não pode ser aceito o valor zero e imprimir o resultado da divisão do primeiro valor lido pelo segundo valor lido. 
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>//biblioteca para utilizar setlocale
+
+int main(){
+	setlocale(LC_ALL,"Portuguese");
+	
+	float n1, n2, div=0;
+	
+	printf("Informe o primeiro valor: ");
+	scanf("%f",&n1);
+	printf("Informe o segundo valor: ");
+	scanf("%f",&n2);
+	
+	if(n2==0){
+		while(n2==0){
+			printf("O segundo valor não aceita 0. Informe outro número!\n");
+			printf("Informe o segundo valor: ");
+			scanf("%f",&n2);
+			system("cls");//limpa tela
+		}
+	}
+	
+	div=n1/n2;
+	printf("----- RESULTADO -----\n\n");
+	printf("Divisão entre %.2f e %.2f: %.2f", n1, n2, div);
+}
---
+++ Exercícios em C/exercicio29.cpp
@@ -0,0 +1,34 @@
+/*Faça um programa para ler uma matriz 3x3 e traga o produto da diagonal principal.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define tam 3//defir o tamanho da matriz
+
+
+int main (){
+	int matriz[tam][tam], i, j, dia=1;
+	
+	printf("Digite o valor dos elemntos da Matriz\n\n");
+	for(i=0; i<tam; i++){
+		for(j=0; j<tam; j++){
+			printf("\n[%d][%d] = ", i, j);
+			scanf("%d",&matriz[i][j]);
+		}//coluna
+	}//linha
+	
+	for(i=0; i<tam; i++){
+		for(j=0; j<tam; j++){
+			if(i==j)
+				dia=dia*matriz[i][j];
+			
+		}//coluna
+	}//linha
+	
+	system("cls");//limpar tela
+	printf("-------- RESULTADO --------\n");
+    printf("\nProduto diagonal principal: %d",dia);
+        
+    return 0;
+	
+}
